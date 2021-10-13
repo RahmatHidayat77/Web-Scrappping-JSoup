4dab5ea0aca662cb32371011e43eda3895287072
Rahmat Hidayat <rh.hie.dayatt@gmail.com>
2021-10-13 22:09:51 +0700

    comment unused depency + check JSoup fuctionality

--- web-srcapping/src/main/java/com/scrapping/App.java
+++ web-srcapping/src/main/java/com/scrapping/App.java
@@ -1,5 +1,8 @@
 package com.scrapping;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  * Hello world!
  */
@@ -11,7 +14,9 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {       
+        final Document document = Jsoup.connect("https://www.tokopedia.com/p/handphone-tablet/handphone").get();
+
+        System.out.println(document.outerHtml());
     }
 }
