92255a93fa473148622ed240a524ad4870e0ccd6
Rahmat Hidayat <rh.hie.dayatt@gmail.com>
2021-10-14 14:51:27 +0700

    trying get some needed data

--- web-srcapping/src/main/java/com/scrapping/App.java
+++ web-srcapping/src/main/java/com/scrapping/App.java
@@ -10,6 +10,7 @@ import com.opencsv.CSVWriter;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * Hello world!
@@ -19,7 +20,7 @@ public final class App {
     }
 
 
-    public static void generateCsvFile() {
+    public static void generateCsvFile(List<String[]> data) {
   
     // first create file object for file placed at location
     // specified by filepath
@@ -32,11 +33,6 @@ public final class App {
         // create CSVWriter object filewriter object as parameter
         CSVWriter writer = new CSVWriter(outputfile);
   
-        // create a List which contains String array
-        List<String[]> data = new ArrayList<String[]>();
-        data.add(new String[] { "Name", "Class", "Marks" });
-        data.add(new String[] { "Aman", "10", "620" });
-        data.add(new String[] { "Suraj", "10", "630" });
         writer.writeAll(data);
   
         // closing writer connection
@@ -53,9 +49,36 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) throws Exception {       
-        // final Document document = Jsoup.connect("https://www.tokopedia.com/p/handphone-tablet/handphone").get();
+        List<String[]> data = new ArrayList<String[]>();
+        data.add(new String[] { "NameOfProduct", "Description", "ImageLink", "Price", "Rating", "NameOfStore" });
 
-        // System.out.println(document.outerHtml());
-        generateCsvFile();
+        for (int i = 1; i <= 15; i++) {
+
+            final Document document = Jsoup.connect("https://www.tokopedia.com/p/handphone-tablet/handphone?page="+i).get();
+
+
+            for (Element e : document.select("div.css-13l3l78.e1nlzfl10 div")) {
+                String nameOfProduct, desc, imageLink, price, rating, nameOfStore;
+                
+                if (!e.select("span.css-1kr22w3").isEmpty() && 
+                    !e.select("span.css-o5uqvq").text().isEmpty() && 
+                    !e.select("span.css-1bjwylw").text().isEmpty() &&
+                    !e.select("div.css-1c0vu8l img").attr("abs:src").isEmpty()) {
+                        
+                    nameOfStore = e.select("span.css-1kr22w3").get(1).text();
+                    price = e.select("span.css-o5uqvq").text();
+                    nameOfProduct = e.select("span.css-1bjwylw").text();
+                    imageLink = e.select("div.css-1c0vu8l img").attr("abs:src"); //.select("img").attr("abs:src");
+
+                    desc = "";
+                    rating = "";
+
+                    if (!data.stream().anyMatch(x -> x[0].equals(nameOfProduct))) data.add(new String[] {nameOfProduct, desc, imageLink, price, rating, nameOfStore});
+                } else {
+                    continue;
+                }  
+            }
+        }
+        generateCsvFile(data);
     }
 }
